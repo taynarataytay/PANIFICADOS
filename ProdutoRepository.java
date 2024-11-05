426778f9720b8c4f76ba97162d3d691e3392111a
taynarataytay <71515330+taynarataytay@users.noreply.github.com>
2024-11-05 09:07:10 -0300

    Create ProdutoRepository.java

---
+++ ProdutoRepository.java
@@ -0,0 +1,9 @@
+package com.exemplo.padaria.repository;
+
+import com.exemplo.padaria.model.Produto;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ProdutoRepository extends JpaRepository<Produto, Integer> {
+}
