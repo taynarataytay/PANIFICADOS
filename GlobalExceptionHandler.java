bb18436a9165c60d445478068d687d873f0a88bf
taynarataytay <71515330+taynarataytay@users.noreply.github.com>
2024-11-05 09:26:12 -0300

    Update GlobalExceptionHandler.java

--- GlobalExceptionHandler.java
+++ GlobalExceptionHandler.java
@@ -1,16 +1,26 @@
-package com.exemplo.padaria.exception;
-
 import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import java.util.HashMap;
+import java.util.Map;
 
-@ControllerAdvice
+@RestControllerAdvice
 public class GlobalExceptionHandler {
 
-    @ExceptionHandler(RuntimeException.class)
-    @ResponseStatus(HttpStatus.NOT_FOUND)
-    public String handleRuntimeException(RuntimeException ex) {
-        return ex.getMessage();
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public Map<String, String> handleValidationExceptions(MethodArgumentNotValidException ex) {
+        Map<String, String> errors = new HashMap<>();
+        ex.getBindingResult().getFieldErrors().forEach(error -> 
+            errors.put(error.getField(), error.getDefaultMessage())
+        );
+        return errors;
+    }
+
+    @ExceptionHandler(Exception.class)
+    public Map<String, String> handleGlobalException(Exception ex) {
+        Map<String, String> error = new HashMap<>();
+        error.put("error", ex.getMessage());
+        return error;
     }
 }
