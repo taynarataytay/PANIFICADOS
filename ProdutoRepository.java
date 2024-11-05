ce02e073441c7e18ab80d55b89e3447de02643a9
taynarataytay <71515330+taynarataytay@users.noreply.github.com>
2024-11-05 09:15:19 -0300

    Update ProdutoRepository.java

--- ProdutoRepository.java
+++ ProdutoRepository.java
@@ -4,6 +4,8 @@ import com.exemplo.padaria.model.Produto;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+List<Produto> findByPrecoBetween(Double min, Double max);
+
 @Repository
 public interface ProdutoRepository extends JpaRepository<Produto, Integer> {
 }
