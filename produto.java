95e40e0121b2b0075245cc0818d51ac742594035
taynarataytay <71515330+taynarataytay@users.noreply.github.com>
2024-11-05 09:33:30 -0300

    Update produto.java

--- produto.java
+++ produto.java
@@ -5,4 +5,23 @@ public class Produto {
     private double preco;
     private String imagem;
 
+    import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Positive;
+
+public class Produto {
+    
+    @NotNull(message = "O ID não pode ser nulo")
+    private Long id;
+
+    @NotBlank(message = "Nome é obrigatório")
+    private String nome;
+
+    @Positive(message = "O preço deve ser maior que zero")
+    private Double preco;
+
+
 }
+
+
+
